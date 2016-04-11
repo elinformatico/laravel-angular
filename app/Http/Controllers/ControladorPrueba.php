9e3479c058690eb563fb35604a47d90ba7a4d792
J.Noe Hernandez Vivanco <donoe1985@gmail.com>
2016-04-11 18:07:00 -0400

    Primera fase funcional con Base de Datos :)

--- app/Http/Controllers/ControladorPrueba.php
+++ app/Http/Controllers/ControladorPrueba.php
@@ -5,10 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use DB;
 
 class ControladorPrueba extends Controller
 {
     public function getMessage(){
-    	return Response()->json(array('msg'=>'Vientos Huracanados. Esta es mi primera API funcional free','error'=>'No Aplica xD'));
+
+	try{
+            $usuarios = DB::table('users')
+	    ->get();
+
+	    return Response()->json(array('msg'=>'Vientos Huracanados. Esta es mi primera API funcional free', 'usuarios' => $usuarios));
+
+        }catch(\Illuminate\Database\QueryException $e){
+            return Response()->json(array('msg'=>'Error de query al consultar los datos.','error'=>$e));
+        }
     }
 }
