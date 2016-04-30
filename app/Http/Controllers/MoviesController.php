530be43731f8bb7a2099ed3f58006543b6396a9b
J.Noe Hernandez V <donoe1985@gmail.com>
2016-04-29 21:58:05 -0500

    Agregamos Alias para la salida del JSON

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -12,7 +12,24 @@ class MoviesController extends Controller
     public function getMovies(){
 
     	try{
-            $movies = DB::table('movies')->skip(0)->take(50)->get();
+            $movies = DB::table('movies')
+            ->select(
+                'mov_id AS idMovie',
+                'mov_title AS title',
+                'mov_dir_id AS idDirector',
+                'mov_year AS year',
+                'mov_released AS released',
+                'mov_runtime AS runtime',
+                'mov_synopsis AS synopsis',
+                'mov_poster AS image',
+                'mov_language AS language',
+                'mov_awards AS awards'
+            )
+            ->where('mov_poster', '<>', 'N/A')
+            ->where('mov_poster', '<>', '')
+            ->skip(0)->take(100)
+            ->get();
+
 	    	return Response()->json(array('status' => 'success', 'movies' => $movies, 'msg' => 'Peliculas Obtenidas exitosamente.'));
 
         }catch(\Illuminate\Database\QueryException $e){
